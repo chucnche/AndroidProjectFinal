62c82eb19ff2fa2d1fd9e3f465c1ca6497b37e08
NGUYENCONGCHUC <nguyencong.chuc@etc.vn>
2020-11-16 09:09:26 +0700

    add menu

--- ProjectAndroid/app/src/main/java/com/example/projectandroid/activity/MainActivity.java
+++ ProjectAndroid/app/src/main/java/com/example/projectandroid/activity/MainActivity.java
@@ -1,45 +1,66 @@
 package com.example.projectandroid.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.projectandroid.R;
 import com.example.projectandroid.database.MyDatabase;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private MyDatabase myDatabase;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        txtUsername=findViewById(R.id.txtUsername);
-//        txtPassword=findViewById(R.id.txtPassword);
-//        checkboxRemember=findViewById(R.id.checkBoxRemember);
-//        btnLogin=findViewById(R.id.btnLogin);
-//        SharedPreferences sharedPreferences = getSharedPreferences("login", Context.MODE_PRIVATE);
-//        final String username = sharedPreferences.getString("username","");
-//        final String password = sharedPreferences.getString("password","");
-//        txtUsername.setText(username);
-//        txtPassword.setText(password);
-//        btnLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String user=txtUsername.getText().toString();
-//                String pass=txtPassword.getText().toString();
-//                myDatabase = Room.databaseBuilder(getBaseContext(), MyDatabase.class, "projectchucnc.db").allowMainThreadQueries().build();
-//                AccountDAO accountDAO=myDatabase.createAccountDAO();
-//                Account account = accountDAO.getAccount(user, pass);
-//                if(account!=null && account.getUsername()!=null){
-//                    Intent intent = new Intent(Login.this,MainActivity.class);
-//                    startActivity(intent);
-//                }else{
-//                    Toast.makeText(getBaseContext(),"Wrong username password",Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                Fragment fragment = null;
+                Intent intent = new Intent(MainActivity.this, ProfileStudentActivity.class);
+                switch (id) {
+                    case R.id.nav_profile:
+                        loadFragment();
+                        drawerLayout.closeDrawers();
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void loadFragment() {
+        ProfileFragment addCatalog = new ProfileFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.frame, addCatalog);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
