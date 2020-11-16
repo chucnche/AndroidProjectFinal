e842a1d297bdece6e380325021ec24feb5282d35
minhdi113 <minhpmhe130249@fpt.edu.vn>
2020-11-17 05:22:10 +0700

    login

---
+++ ProjectAndroid/app/src/main/java/com/example/projectandroid/activity/LoginFormActivity.java
@@ -0,0 +1,16 @@
+package com.example.projectandroid.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectandroid.R;
+
+public class LoginFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+    }
+}
